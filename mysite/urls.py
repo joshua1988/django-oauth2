5bcf9a62c199f68989451a36f482d6286d5e594f
joshua1988 <jangkeehyo@gmail.com>
2016-09-21 17:25:13 +0900

    - django OAuth2 first setup

---
+++ mysite/urls.py
@@ -0,0 +1,8 @@
+from django.conf.urls import include, url
+from django.contrib import admin
+
+urlpatterns = [
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'', include('blog.urls')),
+    url(r'^o/', include('oauth2_provider.urls', namespace='oauth2_provider')),
+]
